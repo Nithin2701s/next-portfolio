a1bd571aa8af6ca536d456f5f730752bb523c7ea
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2024-07-20 23:04:58 +0530

    updates commited

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+export {
+    trailingSlash: true,
+    exportTrailingSlash: true,
+    // Optional: basePath if deploying to a subpath
+    // basePath: '/your-repo-name',
+  };
 export default nextConfig;
