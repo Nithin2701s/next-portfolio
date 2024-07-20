0ebae60bad8e6cb73006a6f5d95807a65e829b4a
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2024-07-20 21:53:32 +0530

    Updates commited

--- src/app/main.ts
+++
@@ -1,9 +0,0 @@
-
-window.onscroll = () => {
-    ScrollReveal({
-        distance : "80px" ,
-        duration : 2000,
-        delay : 200,
-    });  
-    ScrollReveal().reveal('.home-content, heading', { origin : 'top'});  
-}
