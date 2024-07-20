4eb832077eff33f8ef10e3fd8bb21a611d70550f
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2024-07-20 22:08:08 +0530

    update Typed.tsx

--- src/app/components/TypedText.tsx
+++ src/app/components/TypedText.tsx
@@ -1,14 +1,18 @@
 // components/TypedText.js
 "use client";
-import React,{ useEffect, useRef } from 'react';
+import React,{ useEffect, useRef, useState } from 'react';
 import Typed from 'typed.js';
 
 const TypedText = () => {
+  const [isClient, setIsClient] = useState(false);
   const typedElement = useRef<HTMLSpanElement | null>(null);
   const typedInstance = useRef<Typed | null>(null);
-
+  
   useEffect(() => {
-    if (typeof window !== 'undefined' && typedElement.current) {
+    setIsClient(true);
+}, []);
+  useEffect(() => {
+    if (isClient && typedElement.current) {
       typedInstance.current = new Typed(typedElement.current, {
           strings: ['Frontend Developer', 'Web Developer', 'UI/UX Designer'],
           typeSpeed: 70,
@@ -23,8 +27,8 @@ const TypedText = () => {
         typedInstance.current.destroy();
     }
     };
-  }, []);
-
+  }, [isClient]);
+  if (!isClient) return null;
   return <span ref={typedElement} className='text-[#78b6e6]'></span>;
 };
 
