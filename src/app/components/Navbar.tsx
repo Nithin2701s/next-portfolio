0e0cf1ebdbb905bca01fcc95e62eef519e23a532
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2024-07-21 22:16:02 +0530

    Made Navbar responsive

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,22 @@
+'use client'
 import Link from "next/link";
 import '../style.css'
+import { useState } from "react";
 export default function Navbar(){
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
     return (
         <header className="header flex space-x-1 fixed top-0 left-0 w-full justify-between pt-[1.8rem] px-[9%] z-50">
         <Link href="#" className="logo font-semibold text-[1.5rem]">Portfolio</Link>
-        {/* <i className="fa-solid fa-bars" id="menu-icon"></i> */}
-  
-        <nav className="navbar space-x-9 text-[1.2rem] font-[600] py-3">
-          <Link href="#home" className=" link hover:text-[#78b6e6] hover:border-b-1">Home</Link>
+        
+        <button className="hamburger"  onClick={toggleMenu}>
+        <i className="fa-solid fa-bars" id="menu-icon"></i>
+      </button>
+        <nav className={`navbar  space-x-9 text-[1.2rem] font-[600] py-3 ${isOpen ? 'show' : ''}`}>
+          <Link href="#home" className=" link hover:text-[#78b6e6]">Home</Link>
           <Link href="#about" className=" link hover:text-[#78b6e6]">About</Link>
           <Link href="#services" className=" link hover:text-[#78b6e6]">Skills</Link>
           <Link href="#projects" className=" link hover:text-[#78b6e6]">Projects</Link>
